62a0c3f547667631ba88c4cf68c9ffdb623555a0
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-05-05 14:29:09 +0530

    reset password feat.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs';
 import transporter from '../nodemailer/nodemailer.js';
 import { User } from "../models/auth.model.js";
-import { VERIFICATION_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from '../nodemailer/emailTemplates.js';
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from '../nodemailer/emailTemplates.js';
 
 export const register = async (req, res) => {
     try {
@@ -124,6 +124,105 @@ export const verifyEmail = async (req, res) => {
         return res.status(200).json({
             success: true,
             message: "Email verify successfully."
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+            error
+        })
+    }
+}
+
+
+export const sendPasswordResetOtp = async (req, res) => {
+    try {
+        const { email } = req.body;
+        if (!email) {
+            return res.status(400).json({
+                success: false,
+                message: "Email is required!"
+            })
+        }
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Please enter valid email!"
+            })
+        }
+
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        user.resetPasswordToken = otp;
+        user.resetPasswordExpireAt = Date.now() + 15 * 60 * 1000;
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Password reset otp",
+            text: `Your password reset otp is ${otp}`
+            // html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("[Customer Name]", user.name).replace("COMPANY", "MERN-Auth").replace("[COMPANY]", "MERN-Auth"),
+        };
+
+        await transporter.sendMail(mailOptions);
+        return res.status(200).json({
+            success: true,
+            message: "OTP sent to your email."
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+            error
+        })
+    }
+}
+
+export const verifyPasswordResetOtp = async (req, res) => {
+    try {
+        const { newPassword, otp } = req.body;
+        if (!otp) {
+            return res.status(400).json({
+                success: false,
+                message: "Credentials required!"
+            })
+        }
+        const user = await User.findOne({
+            resetPasswordToken: otp,
+            resetPasswordExpireAt: { $gt: Date.now() }
+        })
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid OTP!"
+            })
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(newPassword, salt);
+
+
+        user.password = hash;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpireAt = undefined;
+
+        await user.save();
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Successfully reset password.",
+            // text: `Your password reset otp is ${otp}`
+            html: PASSWORD_RESET_SUCCESS_TEMPLATE
+        };
+
+        await transporter.sendMail(mailOptions);
+        return res.status(200).json({
+            success: true,
+            message: "Password reset successfully."
         })
     } catch (error) {
         return res.status(500).json({
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, logout, register, verifyEmail } from '../controllers/auth.controller.js';
+import { login, logout, register, sendPasswordResetOtp, verifyEmail, verifyPasswordResetOtp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
 router.post('/verify-email', verifyEmail)
+router.post('/send-password-reset-otp', sendPasswordResetOtp);
+router.post('/verify-password-reset-otp', verifyPasswordResetOtp);
 
 export default router;
