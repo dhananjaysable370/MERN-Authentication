1634b417e4fd155a98cbfacbacf38d391a105c8a
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-05-13 20:49:05 +0530

    complete

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.BACKEND_URL,
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
