d581a2321b4324994b5d0ffe6bf8f4b80d0f3eb2
Dhananjay Sable <125184802+dhananjaysable370@users.noreply.github.com>
2025-05-15 17:28:47 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -44,6 +44,20 @@ const Login = () => {
       );
 
       if (data.success) {
+        if (!data.user.isVerified) {
+          toast.warning("Please verify your email first!",{
+          style: {
+            background: "rgba(32, 56, 70, 0.6)",
+            color: "#fff",
+            backdropFilter: "blur(8px)",
+            WebkitBackdropFilter: "blur(8px)",
+            border: "1px solid rgba(255,255,255,0.15)",
+            boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          },
+        })
+          setIsLoggedIn(true);
+          return navigate("/verify-email");
+        }
         toast.success(data.message, {
           style: {
             background: "rgba(32, 56, 70, 0.6)",
@@ -55,11 +69,6 @@ const Login = () => {
           },
         });
         setAuthUser(data.user);
-
-        if (!data.user.isVerified) {
-          setIsLoggedIn(true);
-          return navigate("/verify-email");
-        }
         setIsLoggedIn(true);
         navigate("/dashboard");
       }
