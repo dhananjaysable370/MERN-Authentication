9d5e050639f40e387d1f0abdcb8bc68490dd4fb7
Dhananjay Sable <125184802+dhananjaysable370@users.noreply.github.com>
2025-05-15 17:16:23 +0530

    Update ForgotPassword.jsx

--- frontend/src/Pages/ForgotPassword.jsx
+++ frontend/src/Pages/ForgotPassword.jsx
@@ -37,7 +37,16 @@ const ForgotPassword = () => {
         setIsSubmitted(true);
       }
     } catch (error) {
-      toast.error(error?.response?.data?.message || "Something went wrong.");
+      toast.error(error?.response?.data?.message || "Something went wrong.",{
+          style: {
+            background: "rgba(32, 56, 70, 0.6)",
+            color: "#fff",
+            backdropFilter: "blur(8px)",
+            WebkitBackdropFilter: "blur(8px)",
+            border: "1px solid rgba(255,255,255,0.15)",
+            boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          },
+        });
     }
   };
 
