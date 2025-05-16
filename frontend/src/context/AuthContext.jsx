3d1e4b6dc2e0b41537f544804e262dd0a227385b
Dhananjay Sable <125184802+dhananjaysable370@users.noreply.github.com>
2025-05-16 15:11:43 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -41,6 +41,16 @@ export const AuthProvider = ({ children }) => {
   }
 };
 
+  const toastStyle = {
+    style: {
+      background: "rgba(32, 56, 70, 0.6)",
+      color: "#fff",
+      backdropFilter: "blur(8px)",
+      WebkitBackdropFilter: "blur(8px)",
+      border: "1px solid rgba(255,255,255,0.15)",
+      boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+    },
+  };
   const value = {
     BACKEND_URL,
     authUser,
@@ -54,6 +64,7 @@ export const AuthProvider = ({ children }) => {
     setLoadingSpinner,
     errors,
     setErrors,
+    toastStyle
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
